f2a560ab9ef8ef150ba2632cf00924d04d16c2aa
MariiaTkachenko3108 <mt.mariiatkachenko@gmail.com>
2021-11-04 19:52:37 +0200

    move(x) and operator

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -58,11 +58,51 @@ public:
 		return LongInt(sub((*this).value, x.value));
 	}
 
+	LongInt operator * (LongInt x)
+	{
+		return 
+	}
+
+
+	static string multiply_x10(string num, long steps)
+	{
+		for (int i = 0; i < steps; i++)
+			num.append("0");
+
+		return num;
+	}
+
+
+	static string Karatsuba(LongInt num1, LongInt num2)
+	{
+		LongInt result, a1, a2, b1, b2, c1, c2, T1;
+		long n = LongInt::equalizer(num1.value, num2.value);
+		if (n == 1)
+		{
+			
+		}
+
+		a1 = num1.value.substr(0, n/2);
+		a2 = num1.value.substr(n/2, n - n/2);
+		b1 = num2.value.substr(0, n/2);
+		b2 = num2.value.substr(n/2, n - n/2);
+
+		c1 = a1*b1;
+		c2 = a2*b2;
+
+		T1 = (a1 + a2) * (b1 + b2) - c1 - c2;
+
+		result = c1 + c2 + T1
+		
+			
+		return ();
+
+	}
 
 
 };
 
-// каратсуба:
+// karatsuba:
 // x = aT + b
 // y = cT + d 
 // x*y = acT^2 + (ad + bc)T + bd
