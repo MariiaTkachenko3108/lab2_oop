e4bf7be30db49d03b40c69b1d9401130fbbaa1ac
MariiaTkachenko3108 <mt.mariiatkachenko@gmail.com>
2021-11-04 19:00:35 +0200

    equalizer

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
+#include <string>
+
+
 using namespace std;
 
-template < >
-class longint()
+int base = 10;
+
+
+class LongInt
 {
+public:
+
+
+
+	static int equalizer(string num1, string num2)
+	{
+		int s = max(num1.size(), num2.size());
+
+		while (num1.size() < s)
+			num1.insert(0, "0");
+		while (num2.size() < s)
+			num2.insert(0, "0");
+
+		return 0;
+	}
+
 
 };
+
+// каратсуба:
+// x = aT + b
+// y = cT + d 
+// x*y = acT^2 + (ad + bc)T + bd
+// x*y = acT^2 + ((a+b)(c+d) - ac - bd)T + bd
 
 
 int main()
 {
-
+	return 0;
 }
