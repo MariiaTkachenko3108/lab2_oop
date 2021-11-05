0fd0a0c3971a4a0761fe4e9703d106c117fed69e
MariiaTkachenko3108 <mt.mariiatkachenko@gmail.com>
2021-11-05 12:37:23 +0200

    karatsuba + toom3 (works)

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -173,6 +173,76 @@ public:
 	}
 
 
+	static string Toom3(LongInt a, LongInt b)
+	{
+		LongInt result, a1, a2, a3, b1, b2, b3, c1, c2, c3, d1, d2, d3, T1, T2, T3;
+
+		long m = LongInt::equalizer(a.value, b.value);
+
+		if (m == 1)
+		{
+			return to_string((a.value[0] - '0') * (b.value[0] - '0'));
+		}
+
+
+		if (m % 3)
+		{
+			m += 3 - m % 3;
+
+			while (a.value.size() < m)
+				a.value.insert(0, "0");
+			LongInt::equalizer(a.value, b.value);
+		}
+
+
+
+		a1 = a.value.substr(0, m / 3);
+		a2 = a.value.substr(m / 3, m / 3);
+		a3 = a.value.substr(2 * m / 3, m / 3);
+		b1 = b.value.substr(0, m / 3);
+		b2 = b.value.substr(m / 3, m / 3);
+		b3 = b.value.substr(2 * m / 3, m / 3);
+
+		c1 = Toom3(a1, b1);
+		c2 = Toom3(a2, b2);
+		c3 = Toom3(a3, b3);
+
+		/*
+		(a1+a2)*(b1+b2)
+		(a1+a3)*(b1+b3)
+		(a2+a3)*(b2+b3)
+		*/
+
+
+		d1 = Toom3((a1 + a2), (b1 + b2));
+		d2 = Toom3((a1 + a3), (b1 + b3));
+		d3 = Toom3((a2 + a3), (b2 + b3));
+
+		/*
+		(a1 + a2)*(b1 + b2) - a1 * b1 - a2 * b2
+		(a1 + a3)*(b1 + b3) - a1 * b1 - a3 * b3
+		(a2 + a3)*(b2 + b3) - a2 * b2 - a3 * b3
+		*/
+
+
+		T1 = d1 - c1 - c2;
+		T2 = d2 - c1 - c3;
+		T3 = d3 - c2 - c3;
+
+		c1 = LongInt::multiply_x10(c1.value, 4 * m / 3);
+		c2 = LongInt::multiply_x10(c2.value, 2 * m / 3);
+		T1 = LongInt::multiply_x10(T1.value, 3 * m / 3);
+		T2 = LongInt::multiply_x10(T2.value, 2 * m / 3);
+		T3 = LongInt::multiply_x10(T3.value, m / 3);
+
+
+		result = c1 + c2 + c3 + T1 + T2 + T3;
+
+		return result.value.erase(0, min(result.value.find_first_not_of('0'), result.value.size() - 1));
+
+	}
+
+
 };
 
 
@@ -183,9 +253,11 @@ int main()
 
 	LongInt a, b;
 
-	a = "123456789";
-	b = "123456789987654321";
+	a = "123456";
+	b = "9876543219173";
 
+
+	cout << "Перелік методів:" << "\n1. Множення Каратсуби" << "\n2. Множення Тоома-Кука" << endl << endl;
 	cout << "Введіть номер методу" << endl;
 
 
@@ -199,7 +271,10 @@ int main()
 		break;
 
 	case 2:
+		cout << LongInt::Toom3(a, b) << endl;
 		break;
+	default:
+		cout << "Хибний формат вводу" << endl;
 	}
 	
 
